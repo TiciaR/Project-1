d15c4ccfb11b902f101506412b5f46e1b94668d1
Ticia <ticiarodela@gmail.com>
2020-03-18 20:00:49 -0700

    save

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var firebaseConfig = {
+        apiKey: "AIzaSyALagCWQwYswZCkJEE9Tlh6sjrCW1n_JOw",
+        authDomain: "project-1-44a55.firebaseapp.com",
+        databaseURL: "https://project-1-44a55.firebaseio.com",
+        projectId: "project-1-44a55",
+        storageBucket: "project-1-44a55.appspot.com",
+        messagingSenderId: "412515033355",
+        appId: "1:412515033355:web:2c70e7f225fc2db6d2420e"
+      };
+    }
