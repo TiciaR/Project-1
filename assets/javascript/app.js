936013bf02c06b5b4c38c8b55bd6c808a125fe10
steven Reinart <stevenreinart123@gmail.com>
2020-03-23 19:38:32 -0700

    javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,15 +18,33 @@ var database = firebase.database();
 var name;
 var one;
 var two;
-
+function fetchKantoPokemon(){
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+  .then(response => response.json())
+  .then(allpokemon => console.log(allpokemon))
+}
+ fetchKantoPokemon();
 
 
 //Ebay Ajax call 
 $(document).on("click", "#pokebtn", function (event) {
   event.preventDefault();
+
+  // Ebay API Var
   var cors_api_host = 'cors-anywhere.herokuapp.com';
   var appId = "stevenre-p-PRD-1ef6a212e-d2503091";
-  var queryURL = "https://" + cors_api_host + "/open.api.ebay.com/shopping?callname=FindProducts&responseencoding=XML&appid=" + appId + "&siteid=0&version=967&QueryKeywords=harry%20potter&AvailableItemsOnly=true&MaxEntries=2";
+  var queryURL = "https://" + cors_api_host + "/open.api.ebay.com/shopping?callname=FindProducts&responseencoding=XML&appid=" + appId + "&siteid=0&version=967&QueryKeywords=" + pokeName + "&AvailableItemsOnly=true&MaxEntries=2";
+  var pokeName = $("#search-input").val().trim();
+  // PokeAPI var
+  var queryURL1 = "https://api.pokemontcg.io/v1/cards?name=" + pokeName;
+  // user input
+  var newPokemon = {
+    name: pokeName,
+  };
+  database.ref().push(newPokemon);
+  console.log(newPokemon.name);
+
+  //ebay ajax get method
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -35,30 +53,30 @@ $(document).on("click", "#pokebtn", function (event) {
     var results = response.data;
     console.log(response);
   });
+
+  //pokeAPI
+  // $.ajax({
+  //   url: queryURL1,
+  //   method: "GET"
+  // }).then(function (response) {
+  //   var results = response.data;
+  //   console.log(results);
+  // });
+
+
+
+})
+
+
+
+// //PokemonTCG 
+// $(document).on("click", "#pokebtn", function () {
+//   event.preventDefault();
+//     var title = $(this).attr("data-value");
+//   console.log("title: " + title)
+//   //var apiKey = "Pi3IpyjBiZFkZmSASKn4J57JdmSj6rlf";
   
-  // var input = $("#pokemonebay").val();
-  // topics.push(input);
-  // $("#pokemonebay").val("");
-  // genButtons();
 
-})
+//   // Ajax call 
 
-genButtons();
-
-//PokemonTCG 
-$(document).on("click", ".top-genButtons", function () {
-  $(".giphy-section").empty()
-  var title = $(this).attr("data-value");
-  console.log("title: " + title)
-  //var apiKey = "Pi3IpyjBiZFkZmSASKn4J57JdmSj6rlf";
-  var queryURL = "https://api.pokemontcg.io/v1/cards>"
-
-  // Ajax call 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-    var results = response.data;
-    console.log(results);
-  });
-})
+// })
